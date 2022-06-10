351bc562ee64a22840efcfa30e9f2261972ca5ba
Bharath S <94459111+brion4@users.noreply.github.com>
2022-06-10 16:25:26 +0530

    Update Pgm21.R

--- Pgm21.R
+++ Pgm21.R
@@ -6,5 +6,7 @@ result<-array(c(vect),dim=c(3,2,2),dimnames=list(row.names,column.names,matrix.n
 diff_price<-result[1,2,1]-result[1,1,1]
 avg<-(result[1,2,1]+result[1,1,1])/2
 perc_diff<-100*(diff_price/avg)
-print("The percentage difference is :")
+print('The percentage difference is :')
 print(perc_diff)
+print('The 2018 Apple Stock Price is :' )
+print(result["Stock Prices","2018","Apple"])
