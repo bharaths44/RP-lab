0bc84c97a74475888a43dabf71f41d46c8b823ae
Bharath S <94459111+brion4@users.noreply.github.com>
2022-07-29 16:25:56 +0530

    Delete pgm19.R

--- pgm19.R
+++
@@ -1,7 +0,0 @@
-date<-as.Date("2002/10/23")
-n=273
-olddate<-date-n
-newdate<-date+n
-print(paste("original date is ",date))
-print(paste("Subtracted date is",olddate))
-print(paste("Added date is ",newdate))
