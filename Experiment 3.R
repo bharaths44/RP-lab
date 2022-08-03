d4b980b527430cd53a93ab2d209910dc8f7b2a2a
Bharath S <94459111+brion4@users.noreply.github.com>
2022-08-03 21:06:35 +0530

    Update Experiment 3.R

--- Experiment 3.R
+++ Experiment 3.R
@@ -7,12 +7,12 @@ print("The reversed list is")
 print(result)
 
 #3.2
-x <- c(1,2,3,4,5,6)
+l1<-list(1,2,3,4,5,6)
+x <-unlist(l1)
 print("The original list is ")
-print(x)
-sqr <- sapply(x, function(x) x ^ 2)
-print("The squared vector ")
-print(sqr)
+print(l1)
+for(i in x) x[i]<-x[i]^2
+x
 
 #3.3
 x = list(list(1,2), list(13,4), list(5,26))
@@ -28,7 +28,77 @@ c1 = list("A", "B", "C")
 print("Original lists:")
 print(n1)
 print(c1)
-print("Merge the said lists:")
 mlist=c(n1, c1)
 print("New merged list:")
 print(mlist)
+
+#3.5
+l1<-list(3,6,9)
+print("The list is :")
+l1
+print("The unlisted object is")
+x<-unlist(l1)
+x
+
+#3.6
+data_emp<-data.frame(Name=c("John","Earl","Chris","Joseph","Elliot"),
+    Age=c(30,28,25,27,42),Gender=c("M","M","M","M","M"),
+    Designation=c("CEO","Manager","HR","Salesaman","Director"),
+    Contact=c("9845678902","1234567890","4545451789","4784367893","8123567893"))
+print("Details of the employees :")
+data_emp
+summary(data_emp)
+
+#3.7
+data<-data.frame(Name=c("John","Elliot","Chris"),
+                SGPA=c(10,9.4,9.8),Pass=c("Y","Y","Y"))
+print("Original dataframe")
+data
+print("Statistical summary and nature of dataframe :")
+summary(data)
+str(data)
+
+#3.8
+data_emp<-data.frame(Name=c("John","Earl","Chris","Joseph","Elliot"),
+                     Age=c(30,28,25,27,42),Gender=c("M","M","M","M","M"),
+                     Designation=c("CEO","Manager","HR","Salesaman","Director"),
+                     Contact=c("9845678902","1234567890","4545451789","4784367893","8123567893"))
+print("Original dataframe")
+data_emp
+data_emp<-data_emp[c(3,5),c(1,3)]
+print("Extracted data")
+data_emp
+
+#3.9
+df<-data.frame("c1"=c(1,2,3,4),"c2"=c(5,6,7,8))
+df
+df[nrow(df)+1,]<-c(9,10)
+df
+
+#3.10
+data<-data.frame(Name=c("John","Elliot","Chris"),
+                 SGPA=c(10,9.4,9.8),Pass=c("Y","Y","Y"))
+print("Original dataframe")
+data
+data<-data[with(data,order(SGPA)),]
+print("Soreted dataframe is :")
+data
+
+#3.11
+data<-read.csv("empdata.csv")
+data
+print("The max salary is :")
+sal <- max(data$salary)
+print(sal)
+print("The Person with max salary is")
+val <- subset(data, salary == max(salary))
+print(val)
+print("People in IT department")
+val <- subset( data, dept == "IT")
+print(val)
+print("People joined on or after 2014")
+val <- subset(data, as.Date(start_date) > as.Date("2014-01-01"))
+print(val)
+write.csv(val,"output.csv")
+newdata <- read.csv("output.csv")
+print(newdata)
