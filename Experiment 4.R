3291c6a9bf01b88415e670d5e1d24f62169ee77f
Bharath S <94459111+brion4@users.noreply.github.com>
2022-08-17 22:05:27 +0530

    Add files via upload

--- Experiment 4.R
+++ Experiment 4.R
@@ -1,7 +1,7 @@
 #4.1
 data("iris")
 library(ggplot2)
-ggplot(iris,aes(x=Petal.Length,y=Petal.Width))+geom_point(aes(color=factor(Species)))+geom_smooth(method="lm")+geom_label(aes(label=Species,hjust=0),nudge_y=0.5,size=2)
+ggplot(iris,aes(x=Petal.Length,y=Petal.Width))+geom_point(aes(color=factor(Species)))+geom_smooth(method="lm")+geom_label(aes(label=Species,hjust=0),nudge_y=0.1,size=2)
 
 #4.2
 library(ggplot2)
--- Experiment 5.R
+++ Experiment 5.R
@@ -3,15 +3,13 @@ library(ggplot2)
 tv<-read.csv("tv.csv")
 data(tv)
 relation <- lm(Sales~TV,data=tv)
-summary(relation)
-ggplot(relation,aes(TV,Sales))+ geom_jitter(aes(TV,Sales),size=1,color="black")+geom_smooth(aes(TV,Sales),method="lm",se=FALSE,color="black",size=0.5)
+relation
+plot(relation)
 
 #5.2
 library(ggplot2)
 jobprof<-read.csv("jobprof.csv")
 data(jobprof)
 model <- lm(y~x1+x2+x3,data=jobprof)
-summary(model)
-p<-ggplot(model)+geom_jitter(aes(x1,y),colour="red")+geom_smooth(aes(x1,y,col="x1"),method="lm",se=FALSE)+geom_jitter(aes(x2,y),colour="green")+geom_smooth(aes(x2,y,col="x2"),method="lm",se=FALSE)+
-   geom_jitter(aes(x3,y),colour="blue")+geom_smooth(aes(x3,y,col="x3"),method="lm",se=FALSE)
-p+ggtitle("Multple Linear Regression")+xlab("X")+ylab("Y")
+model
+plot(model)
